4dd136b47f7cfbe4e4808efbf1e7738cd286279c
shubhampachghare <shubhampachghare@gmail.com>
2023-07-18 02:10:54 +0530

    [Shubham] -Examples

---
+++ FileIO/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace FileIO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path =@"C:\RFP 289\FileIO\FileIO\abc.txt";
+            if(File.Exists(path))
+            {
+                Console.WriteLine("File Exists");
+
+            }
+            else
+            {
+                Console.WriteLine("File Not Exists");
+            }
+
+            string[] lines;
+            string lines2;
+            lines = File.ReadAllLines(path);
+
+             Console.WriteLine(lines[0]);
+             Console.WriteLine(lines[1]);
+            Console.WriteLine("-----------------");
+            lines2 = File.ReadAllText(path);
+            Console.WriteLine(lines2);
+        }
+    }
+}
